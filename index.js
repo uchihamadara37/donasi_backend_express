68fddfedbc6c04cb47582085b705d1ec9f216d46
andre <andreamusholin@gmail.com>
2025-05-26 09:12:53 +0700

    okoko

--- index.js
+++ index.js
@@ -58,4 +58,4 @@ const PORT = process.env.PORT || 3000;
 
 // app.listen(PORT, () => console.log('Server running on port 3000'));
 
-https.createServer(options, app).listen(PORT, () => console.log('Server running on port 3000'));
+https.createServer(options, app).listen(PORT, () => console.log(`Server running on port ${PORT}`));
